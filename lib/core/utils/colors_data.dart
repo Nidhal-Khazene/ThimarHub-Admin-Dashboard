1225f1c27f4802f6047484f943d264ab7d3a6df7
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-25 08:41:45 +0100

    add colors data

---
+++ lib/core/utils/colors_data.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract class ColorsData {
+  static Color kPrimaryColor = const Color(0xff1B5E37);
+  static Color kLightPrimaryColor = const Color(0xff288F54);
+  static Color kSecondaryColor = const Color(0xffF4A91F);
+  static Color kLightSecondaryColor = const Color(0xFFF8C76D);
+  static Color kFontSecondaryColor = const Color(0xFF949D9E);
+}
