00e3a1f9ecdd40b4994c26262ad5412e46496b7f
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-26 20:43:57 +0100

    add product view body: add products fields

--- lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
+++ lib/features/add_product/presentation/views/widgets/add_product_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app_dashboard/core/widgets/custom_text_form_field.dart';
+import 'package:ecommerce_app_dashboard/core/widgets/image_field.dart';
 import 'package:flutter/material.dart';
 
 class AddProductViewBody extends StatefulWidget {
@@ -17,7 +19,33 @@ class _AddProductViewBodyState extends State<AddProductViewBody> {
       child: Form(
         key: _formKey,
         autovalidateMode: autoValidateMode,
-        child: Column(children: []),
+        child: Column(
+          children: [
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: "Product Name",
+              textInputType: TextInputType.text,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: "Product Price",
+              textInputType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: "Product Code",
+              textInputType: TextInputType.name,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: "Product Description",
+              textInputType: TextInputType.text,
+              maxLines: 5,
+            ),
+            SizedBox(height: 16),
+            ImageField(),
+          ],
+        ),
       ),
     );
   }
