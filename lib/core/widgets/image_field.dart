2a4bd1dc574542be322e62b5cf1a32b4ad439988
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-27 10:52:10 +0100

    image field: implement skeltenizer in image field

--- lib/core/widgets/image_field.dart
+++ lib/core/widgets/image_field.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_app_dashboard/core/utils/app_styles.dart';
 import 'package:ecommerce_app_dashboard/core/utils/colors_data.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class ImageField extends StatefulWidget {
   const ImageField({super.key});
@@ -11,6 +12,7 @@ class ImageField extends StatefulWidget {
 }
 
 class _ImageFieldState extends State<ImageField> {
+  late bool isLoading = true;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,18 +24,30 @@ class _ImageFieldState extends State<ImageField> {
       ),
       child: GestureDetector(
         onTap: () async {
+          setState(() {
+            isLoading = true;
+          });
           final ImagePicker picker = ImagePicker();
           final XFile? image = await picker.pickImage(
             source: ImageSource.gallery,
           );
+          setState(() {
+            isLoading = false;
+          });
         },
         child: Center(
           child: Column(
             children: [
-              Icon(
-                Icons.image_rounded,
-                size: 180,
-                color: const Color(0xFFC9CECE),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Skeletonizer(
+                  enabled: isLoading,
+                  child: Icon(
+                    Icons.image_rounded,
+                    size: 180,
+                    color: const Color(0xFFC9CECE),
+                  ),
+                ),
               ),
               Text(
                 "Product Image",
