76c2c4af65f12ff8ea68957c907bcea6323d298f
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-28 10:23:03 +0100

    show snack bar: edit the text style of the error message

--- lib/core/helper/show_snack_bar.dart
+++ lib/core/helper/show_snack_bar.dart
@@ -1,5 +1,8 @@
+import 'package:ecommerce_app_dashboard/core/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+  ScaffoldMessenger.of(
+    context,
+  ).showSnackBar(SnackBar(content: Text(message, style: AppStyles.bold16)));
 }
