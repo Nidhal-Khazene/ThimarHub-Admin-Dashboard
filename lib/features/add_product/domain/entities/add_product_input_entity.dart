cf9f1cc0a77b5f5c4f0826a9a99261f8393d58d6
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-28 11:22:36 +0100

    add_product_input_entity added

---
+++ lib/features/add_product/domain/entities/add_product_input_entity.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+class AddProductInputEntity {
+  final String productName, productCode, productDescription;
+  final String? urlImage;
+  final num productPrice;
+  final File fileImage;
+  final bool isFeatured;
+
+  AddProductInputEntity({
+    required this.productName,
+    required this.productCode,
+    required this.productDescription,
+    this.urlImage,
+    required this.productPrice,
+    required this.fileImage,
+    required this.isFeatured,
+  });
+}
