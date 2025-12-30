cfee58a1353d4b490debc960204cbb9514f8ec50
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-30 22:07:59 +0100

    add database service

---
+++ lib/core/services/database_service.dart
@@ -0,0 +1,15 @@
+abstract class DatabaseService {
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+    required String? documentId,
+  });
+  Future<Map<String, dynamic>> getData({
+    required String documentsId,
+    required String path,
+  });
+  Future<bool> checkIfDataExists({
+    required String documentId,
+    required String path,
+  });
+}
