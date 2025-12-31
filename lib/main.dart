f77847f56eefdf5ab15a0be4cd8921b4db9bc10f
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-31 09:16:33 +0100

    main: init to GetItService and Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:ecommerce_app_dashboard/core/routing/on_generate_route.dart';
+import 'package:ecommerce_app_dashboard/core/services/custom_bloc_observer.dart';
+import 'package:ecommerce_app_dashboard/core/services/get_it_service.dart';
 import 'package:ecommerce_app_dashboard/features/dashboard/presentation/views/dashboard_view.dart';
+import 'package:ecommerce_app_dashboard/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = CustomBlocObserver();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  setupGetIt();
+
   runApp(const MyApp());
 }
 
