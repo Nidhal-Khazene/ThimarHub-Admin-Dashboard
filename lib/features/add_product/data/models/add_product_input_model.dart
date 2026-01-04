007c95f4731b4926c69debade74539f7591bf661
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-04 10:24:16 +0100

    add product input model: fix it by adding urlImage to fromEntity and toJson

--- lib/features/add_product/data/models/add_product_input_model.dart
+++ lib/features/add_product/data/models/add_product_input_model.dart
@@ -1,44 +1,41 @@
-import 'dart:io';
-
 import 'package:ecommerce_app_dashboard/features/add_product/domain/entities/add_product_input_entity.dart';
 
 class AddProductInputModel {
-  final String productName, productCode, productDescription;
-  String? urlImage;
+  final String productName;
+  final String productCode;
+  final String productDescription;
   final num productPrice;
-  final File fileImage;
   final bool isFeatured;
+  final String? urlImage; // store URL only
 
   AddProductInputModel({
     required this.productName,
     required this.productCode,
     required this.productDescription,
-    this.urlImage,
     required this.productPrice,
-    required this.fileImage,
     required this.isFeatured,
+    this.urlImage,
   });
 
-  factory AddProductInputModel.fromEntity(
-    AddProductInputEntity addProductInputEntity,
-  ) {
+  factory AddProductInputModel.fromEntity(AddProductInputEntity entity) {
     return AddProductInputModel(
-      productName: addProductInputEntity.productName,
-      productCode: addProductInputEntity.productCode,
-      productDescription: addProductInputEntity.productDescription,
-      productPrice: addProductInputEntity.productPrice,
-      fileImage: addProductInputEntity.fileImage,
-      isFeatured: addProductInputEntity.isFeatured,
+      productName: entity.productName,
+      productCode: entity.productCode,
+      productDescription: entity.productDescription,
+      productPrice: entity.productPrice,
+      isFeatured: entity.isFeatured,
+      urlImage: entity.urlImage,
     );
   }
-  toJson() {
+
+  Map<String, dynamic> toJson() {
     return {
       "productName": productName,
       "productCode": productCode,
       "productDescription": productDescription,
       "productPrice": productPrice,
-      "fileImage": fileImage,
       "isFeatured": isFeatured,
+      "urlImage": urlImage,
     };
   }
 }
