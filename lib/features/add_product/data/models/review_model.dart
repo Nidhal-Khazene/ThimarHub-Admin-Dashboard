6ea05bdcaed936effa01c969d0e49735f1781513
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-05 11:32:30 +0100

    add review model

---
+++ lib/features/add_product/data/models/review_model.dart
@@ -0,0 +1,47 @@
+import 'package:ecommerce_app_dashboard/features/add_product/domain/entities/review_entity.dart';
+
+class ReviewModel {
+  final String name;
+  final String image;
+  final num rating;
+  final String reviewDescription;
+  final String date;
+
+  ReviewModel({
+    required this.name,
+    required this.image,
+    required this.rating,
+    required this.reviewDescription,
+    required this.date,
+  });
+
+  factory ReviewModel.fromEntity(ReviewEntity entity) {
+    return ReviewModel(
+      name: entity.name,
+      image: entity.image,
+      rating: entity.rating,
+      reviewDescription: entity.reviewDescription,
+      date: entity.date,
+    );
+  }
+
+  factory ReviewModel.fromJson(Map<String, dynamic> json) {
+    return ReviewModel(
+      name: json["name"],
+      image: json["image"],
+      rating: json["rating"],
+      reviewDescription: json["reviewDescription"],
+      date: json["date"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "image": image,
+      "rating": rating,
+      "reviewDescription": reviewDescription,
+      "date": date,
+    };
+  }
+}
