77a353acd224bcfada1393d14f620a09196a0076
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-06 22:30:35 +0100

    main: init supabase

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,13 @@ import 'package:ecommerce_app_dashboard/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  await Supabase.initialize(
+    url: 'https://xyzcompany.supabase.co',
+    anonKey: 'publishable-or-anon-key',
+  );
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = CustomBlocObserver();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
