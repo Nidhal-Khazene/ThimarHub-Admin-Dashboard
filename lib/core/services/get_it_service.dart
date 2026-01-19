09dbbdb6fb531ff05a6a9862bcded58e0a3f18b3
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-19 15:28:53 +0100

    replace firebase cloud storage to supabase storage

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -3,16 +3,16 @@ import 'package:ecommerce_app_dashboard/core/repositories/product_repo_impl.dart
 import 'package:ecommerce_app_dashboard/core/services/database_service.dart';
 import 'package:ecommerce_app_dashboard/core/services/firestore_service.dart';
 import 'package:ecommerce_app_dashboard/core/services/storage_service.dart';
+import 'package:ecommerce_app_dashboard/core/services/supabase_storage_service.dart';
 import 'package:get_it/get_it.dart';
 
 import '../repositories/images_repo.dart';
 import '../repositories/images_repo_impl.dart';
-import 'firebase_cloud_storage.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
-  getIt.registerSingleton<StorageService>(FirebaseCloudStorage());
+  getIt.registerSingleton<StorageService>(SupabaseStorageService());
   getIt.registerSingleton<DatabaseService>(FirestoreService());
 
   getIt.registerSingleton<ImagesRepo>(ImagesRepoImpl(getIt<StorageService>()));
