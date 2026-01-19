58740d054c7f9dbe60eb97f1f4fcf55cf7c5262f
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-19 15:28:16 +0100

    supabase_storage_service: implement upload method

--- lib/core/services/supabase_storage_service.dart
+++ lib/core/services/supabase_storage_service.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:ecommerce_app_dashboard/config/urls.dart';
 import 'package:ecommerce_app_dashboard/core/services/storage_service.dart';
+import 'package:ecommerce_app_dashboard/core/utils/backend_break_point.dart';
+import 'package:path/path.dart' as p;
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../config/api_keys.dart';
@@ -17,8 +19,11 @@ class SupabaseStorageService implements StorageService {
   }
 
   @override
-  Future<String> uploadFile(File file, String path) {
-    // TODO: implement uploadFile
-    throw UnimplementedError();
+  Future<String> uploadFile(File file, String path) async {
+    final fileName = p.basename(file.path);
+    var result = await _supabase.client.storage
+        .from(BackendBreakPoint.images)
+        .upload("$path/$fileName", file);
+    return result;
   }
 }
