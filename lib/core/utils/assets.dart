2299542cdf64a5455f2dece6e4483f9d63e43e9e
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-24 13:04:09 +0100

    add dashboard view body items images

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String imagesDashboardChecked = "assets/images/dashboard/checked.svg";
+
+  static const String imagesDashboardOpenCardboardBox = "assets/images/dashboard/open_cardboard_box.svg";
+
+  static const String imagesDashboardShippingCar = "assets/images/dashboard/shipping_car.svg";
+
+  static const String imagesDashboardShippingChart = "assets/images/dashboard/shipping_chart.svg";
+}
+
