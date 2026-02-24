54d7a78bfbc53be76636b9a1e140c3b01723e259
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-24 08:45:54 +0100

    get it service: turn to firebase cloud storage

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,9 +1,9 @@
 import 'package:ecommerce_app_dashboard/core/repositories/product_repo.dart';
 import 'package:ecommerce_app_dashboard/core/repositories/product_repo_impl.dart';
 import 'package:ecommerce_app_dashboard/core/services/database_service.dart';
+import 'package:ecommerce_app_dashboard/core/services/firebase_cloud_storage.dart';
 import 'package:ecommerce_app_dashboard/core/services/firestore_service.dart';
 import 'package:ecommerce_app_dashboard/core/services/storage_service.dart';
-import 'package:ecommerce_app_dashboard/core/services/supabase_storage_service.dart';
 import 'package:get_it/get_it.dart';
 
 import '../repositories/images_repo.dart';
@@ -12,7 +12,7 @@ import '../repositories/images_repo_impl.dart';
 final getIt = GetIt.instance;
 
 void setupGetIt() {
-  getIt.registerSingleton<StorageService>(SupabaseStorageService());
+  getIt.registerSingleton<StorageService>(FirebaseCloudStorage());
   getIt.registerSingleton<DatabaseService>(FirestoreService());
 
   getIt.registerSingleton<ImagesRepo>(ImagesRepoImpl(getIt<StorageService>()));
