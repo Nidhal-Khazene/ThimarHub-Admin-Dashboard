65b92e86d74f6515c95c079f07d29c05e18e90ba
nidhal khazene <nidhal.khazene@gmail.com>
2026-02-24 08:44:31 +0100

    colors data: add kFontPrimaryColor

--- lib/core/utils/colors_data.dart
+++ lib/core/utils/colors_data.dart
@@ -5,6 +5,7 @@ abstract class ColorsData {
   static Color kLightPrimaryColor = const Color(0xff288F54);
   static Color kSecondaryColor = const Color(0xffF4A91F);
   static Color kLightSecondaryColor = const Color(0xFFF8C76D);
+  static Color kFontPrimaryColor = const Color(0xFF0C0D0D);
   static Color kFontSecondaryColor = const Color(0xFF949D9E);
   static Color kBorderColor = const Color(0xFFE6E9E9);
   static Color kContainerFieldColor = const Color(0xffF9FAFA);
