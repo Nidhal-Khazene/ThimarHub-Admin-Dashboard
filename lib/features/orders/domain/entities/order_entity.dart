309d8d63f1e3a11fb090ce1ce80ae0227df70a04
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-01 18:25:35 +0100

    order entity: import recommended packages

--- lib/features/orders/domain/entities/order_entity.dart
+++ lib/features/orders/domain/entities/order_entity.dart
@@ -1,4 +1,6 @@
-ss_entity.dart';
+import 'package:ecommerce_app_dashboard/features/orders/domain/entities/payment_card_entity.dart';
+import 'package:ecommerce_app_dashboard/features/orders/domain/entities/product_order_entity.dart';
+import 'package:ecommerce_app_dashboard/features/orders/domain/entities/shipping_address_entity.dart';
 
 class OrderEntity {
   final double totalPrice;
