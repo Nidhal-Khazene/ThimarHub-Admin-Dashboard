cb0a020f395000ef561826921d14e5c57fd7be02
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-02 09:55:02 +0100

    fetch orders cubit: add fetch orders method

--- lib/features/orders/presentation/manager/cubits/fetch_orders_cubit/fetch_orders_cubit.dart
+++ lib/features/orders/presentation/manager/cubits/fetch_orders_cubit/fetch_orders_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:ecommerce_app_dashboard/features/orders/domain/repos/orders_repo.dart';
 import 'package:meta/meta.dart';
+
+import '../../../../domain/entities/order_entity.dart';
 
 part 'fetch_orders_state.dart';
 
 class FetchOrdersCubit extends Cubit<FetchOrdersState> {
-  FetchOrdersCubit() : super(FetchOrdersInitial());
+  FetchOrdersCubit(this.ordersRepo) : super(FetchOrdersInitial());
+  final OrdersRepo ordersRepo;
+
+  Future<void> fetchOrders() async {
+    emit(FetchOrdersLoading());
+    final result = await ordersRepo.fetchOrders();
+    result.fold(
+      (failure) => emit(FetchOrdersFailure(errorMessage: failure.message)),
+      (orders) => emit(FetchOrdersSuccess(orders: orders)),
+    );
+  }
 }
