ed60dda29426ece08b4b6b13f4414c46dc5191ee
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-03 08:39:51 +0100

    get dummy order: change to OrderEntity

--- lib/core/helper/get_dummy_order.dart
+++ lib/core/helper/get_dummy_order.dart
@@ -1,11 +1,12 @@
-import '../../features/orders/data/models/order_model.dart';
-import '../../features/orders/data/models/payment_card_model.dart';
-import '../../features/orders/data/models/product_order_model.dart';
-import '../../features/orders/data/models/shipping_address_model.dart';
+import 'package:ecommerce_app_dashboard/features/orders/domain/entities/order_entity.dart';
+import 'package:ecommerce_app_dashboard/features/orders/domain/entities/payment_card_entity.dart';
+import 'package:ecommerce_app_dashboard/features/orders/domain/entities/product_order_entity.dart';
+import 'package:ecommerce_app_dashboard/features/orders/domain/entities/shipping_address_entity.dart';
+
 import '../utils/assets.dart';
 
-OrderModel getDummyOrder() {
-  final shippingAddress = ShippingAddressModel(
+OrderEntity getDummyOrder() {
+  final shippingAddress = ShippingAddressEntity(
     id: 1,
     fullName: "John Doe",
     email: "john.doe@email.com",
@@ -15,14 +16,14 @@ OrderModel getDummyOrder() {
   );
 
   final products = [
-    OrderProductsModel(
+    OrderProductsEntity(
       productCode: "PRD-001",
       productName: "اناناس",
       productPrice: 120.0,
       imageUrl: Assets.imagesProductsAnanas,
       quantity: 1,
     ),
-    OrderProductsModel(
+    OrderProductsEntity(
       productCode: "PRD-002",
       productName: "افوكادو",
       productPrice: 80.0,
@@ -31,7 +32,7 @@ OrderModel getDummyOrder() {
     ),
   ];
 
-  final paymentCard = PaymentCardModel(
+  final paymentCard = PaymentCardEntity(
     name: "John Doe",
     cardNumber: "4242424242424242",
     expirationCard: "12/28",
@@ -43,12 +44,12 @@ OrderModel getDummyOrder() {
     (sum, item) => sum + (item.productPrice * item.quantity),
   );
 
-  return OrderModel(
+  return OrderEntity(
     uID: "USER-12345",
     totalPrice: total,
-    shippingAddressModel: shippingAddress,
+    shippingAddressEntity: shippingAddress,
     orderProducts: products,
-    paymentCardModel: paymentCard,
+    paymentCardEntity: paymentCard,
     paymentMethod: "Credit Card",
     status: "pending",
     date: DateTime.now().toString(),
