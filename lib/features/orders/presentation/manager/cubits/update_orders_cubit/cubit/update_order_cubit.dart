81d30e8863eb39dba9f5316323036199be7e44fa
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-10 09:09:16 +0100

    orders view: provide update orders cubit

--- lib/features/orders/presentation/manager/cubits/update_orders_cubit/cubit/update_order_cubit.dart
+++ lib/features/orders/presentation/manager/cubits/update_orders_cubit/cubit/update_order_cubit.dart
@@ -1,13 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:ecommerce_app_dashboard/core/enums/order_status_enum.dart';
-import 'package:ecommerce_app_dashboard/features/orders/data/repos/orders_repo_impl.dart';
 import 'package:ecommerce_app_dashboard/features/orders/domain/repos/orders_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'update_order_state.dart';
 
 class UpdateOrderCubit extends Cubit<UpdateOrderState> {
-  UpdateOrderCubit(this.ordersRepo) : super(UpdateOrderInitial());
+  UpdateOrderCubit({required this.ordersRepo}) : super(UpdateOrderInitial());
   final OrdersRepo ordersRepo;
   Future<void> updateOrder({
     required OrderStatusEnum status,
--- lib/features/orders/presentation/views/orders_view.dart
+++ lib/features/orders/presentation/views/orders_view.dart
@@ -4,6 +4,7 @@ import 'package:ecommerce_app_dashboard/core/services/get_it_service.dart';
 import 'package:ecommerce_app_dashboard/core/utils/colors_data.dart';
 import 'package:ecommerce_app_dashboard/features/orders/domain/repos/orders_repo.dart';
 import 'package:ecommerce_app_dashboard/features/orders/presentation/manager/cubits/fetch_orders_cubit/fetch_orders_cubit.dart';
+import 'package:ecommerce_app_dashboard/features/orders/presentation/manager/cubits/update_orders_cubit/cubit/update_order_cubit.dart';
 import 'package:ecommerce_app_dashboard/features/orders/presentation/views/widgets/orders_view_body_bloc_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,9 +26,17 @@ class OrdersView extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: kPrimaryScreenPadding,
-          child: BlocProvider(
-            create: (context) =>
-                FetchOrdersCubit(ordersRepo: getIt<OrdersRepo>()),
+          child: MultiBlocProvider(
+            providers: [
+              BlocProvider(
+                create: (context) =>
+                    FetchOrdersCubit(ordersRepo: getIt<OrdersRepo>()),
+              ),
+              BlocProvider(
+                create: (context) =>
+                    UpdateOrderCubit(ordersRepo: getIt<OrdersRepo>()),
+              ),
+            ],
             child: OrderViewBodyBlocBuilder(),
           ),
         ),
