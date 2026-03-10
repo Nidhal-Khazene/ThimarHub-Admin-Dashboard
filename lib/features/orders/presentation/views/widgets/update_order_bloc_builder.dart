5a50a2c1734daea82ea32023c746b661bf74c00f
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-10 09:30:37 +0100

    add UpdateOrderBlocBuilder widget

---
+++ lib/features/orders/presentation/views/widgets/update_order_bloc_builder.dart
@@ -0,0 +1,31 @@
+import 'package:ecommerce_app_dashboard/core/helper/get_dummy_order.dart';
+import 'package:ecommerce_app_dashboard/core/helper/show_snack_bar.dart';
+import 'package:ecommerce_app_dashboard/features/orders/presentation/manager/cubits/update_orders_cubit/cubit/update_order_cubit.dart';
+import 'package:ecommerce_app_dashboard/features/orders/presentation/views/widgets/orders_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class UpdateOrderBlocBuilder extends StatelessWidget {
+  const UpdateOrderBlocBuilder({super.key, required this.child});
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<UpdateOrderCubit, UpdateOrderState>(
+      listener: (context, state) {
+        if (state is UpdateOrderFailure) {
+          showSnackBar(context, state.errMessage);
+        }
+        if (state is UpdateOrderSuccess) {
+          showSnackBar(context, "نجاح عملية تحديث حالة الطلب");
+        }
+      },
+      builder: (context, state) {
+        return Skeletonizer(
+          enabled: state is UpdateOrderLoading,
+          child: OrdersViewBody(orders: [getDummyOrder(), getDummyOrder()]),
+        );
+      },
+    );
+  }
+}
