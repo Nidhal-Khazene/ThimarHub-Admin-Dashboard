07b64a913be0ee3b74cc6098452a061761ec5169
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-14 09:50:05 +0100

    add_new_product_view_body: add text fields

--- lib/features/products_management/presentation/views/widgets/add_new_product_view_body.dart
+++ lib/features/products_management/presentation/views/widgets/add_new_product_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app_dashboard/core/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class AddNewProductViewBody extends StatelessWidget {
@@ -5,6 +6,36 @@ class AddNewProductViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          CustomTextFormField(hintText: "اسم الصنف"),
+          SizedBox(height: 8),
+          Row(
+            children: [
+              Expanded(child: CustomTextFormField(hintText: "تاريخ الانتاج")),
+              const SizedBox(width: 16),
+              Expanded(
+                child: CustomTextFormField(hintText: "تاريخ انتهاء الصلاحيه"),
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(child: CustomTextFormField(hintText: "السعر")),
+              const SizedBox(width: 16),
+              Expanded(child: CustomTextFormField(hintText: "خصم%")),
+            ],
+          ),
+          SizedBox(height: 16),
+          CustomTextFormField(
+            hintText: "تفاصيل عنه",
+            maxLines: 5,
+            hintPadding: const EdgeInsets.only(top: 32, right: 20),
+          ),
+        ],
+      ),
+    );
   }
 }
