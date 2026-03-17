286903b74590f9273f7b8703fcb9238cc376cb5f
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-17 10:31:35 +0100

    edit product information view body: add some attributes like form fields variables, formKey...

--- lib/features/products_management/presentation/views/widgets/edit_product_information_view_body.dart
+++ lib/features/products_management/presentation/views/widgets/edit_product_information_view_body.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:ecommerce_app_dashboard/core/utils/app_styles.dart';
 import 'package:ecommerce_app_dashboard/core/utils/colors_data.dart';
 import 'package:ecommerce_app_dashboard/core/widgets/custom_button.dart';
@@ -6,7 +8,10 @@ import 'package:ecommerce_app_dashboard/features/products_management/domain/enti
 import 'package:ecommerce_app_dashboard/features/products_management/presentation/views/widgets/edit_image_section.dart';
 import 'package:flutter/material.dart';
 
-class EditProductInformationViewBody extends StatelessWidget {
+import 'is_featured_field.dart';
+import 'is_organic_field.dart';
+
+class EditProductInformationViewBody extends StatefulWidget {
   const EditProductInformationViewBody({
     super.key,
     required this.productEntity,
@@ -14,68 +19,138 @@ class EditProductInformationViewBody extends StatelessWidget {
   final ProductEntity productEntity;
 
   @override
+  State<EditProductInformationViewBody> createState() =>
+      _EditProductInformationViewBodyState();
+}
+
+class _EditProductInformationViewBodyState
+    extends State<EditProductInformationViewBody> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+  late String productName, productCode, productDescription;
+  late num productPrice;
+  File? fileImage;
+  bool isFeatured = false;
+  bool isOrganic = false;
+  late int expirationsMonth, numberOfCalories, unitAmount;
+
+  @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          CustomTextFormField(hintText: productEntity.productName),
-          SizedBox(height: 8),
-          CustomTextFormField(
-            hintText: "السعر: ${productEntity.productPrice} دينار ",
-          ),
-          SizedBox(height: 8),
-          Row(
-            children: [
-              Expanded(
-                child: CustomTextFormField(hintText: productEntity.productCode),
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: CustomTextFormField(
-                  hintText: productEntity.expirationsMonth.toString(),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 16),
-          Row(
-            children: [
-              Expanded(
-                child: CustomTextFormField(
-                  hintText: productEntity.unitAmount.toString(),
-                ),
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: CustomTextFormField(
-                  hintText: productEntity.numberOfCalories.toString(),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 16),
-          CustomTextFormField(
-            hintText: productEntity.productDescription,
-            maxLines: 5,
-            hintPadding: const EdgeInsets.only(top: 32, right: 20),
-          ),
-          SizedBox(height: 16),
-          EditImageSection(productEntity: productEntity),
-          SizedBox(height: 16),
-          CustomButton(
-            text: "حفظ البيانات",
-            textStyle: AppStyles.bold13.copyWith(color: Colors.white),
-          ),
-          SizedBox(height: 16),
-          CustomButton(
-            text: "لا ارغب",
-            borderColor: ColorsData.kPrimaryColor,
-            backgroundColor: Colors.transparent,
-            textStyle: AppStyles.bold13.copyWith(
-              color: ColorsData.kPrimaryColor,
+      child: Form(
+        key: _formKey,
+        autovalidateMode: autoValidateMode,
+        child: Column(
+          children: [
+            CustomTextFormField(
+              onSaved: (value) {
+                productName = value!;
+              },
+              textInputType: TextInputType.text,
+              hintText: widget.productEntity.productName,
             ),
-          ),
-        ],
+            SizedBox(height: 8),
+            CustomTextFormField(
+              onSaved: (value) {
+                productPrice = num.parse(value!);
+              },
+              textInputType: TextInputType.number,
+              hintText: "السعر: ${widget.productEntity.productPrice} دينار ",
+            ),
+            SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: CustomTextFormField(
+                    onSaved: (value) {
+                      productCode = value!.toLowerCase();
+                    },
+                    textInputType: TextInputType.text,
+                    hintText: widget.productEntity.productCode,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: CustomTextFormField(
+                    onSaved: (value) {
+                      expirationsMonth = int.parse(value!);
+                    },
+                    textInputType: TextInputType.number,
+                    hintText: widget.productEntity.expirationsMonth.toString(),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                Expanded(
+                  child: CustomTextFormField(
+                    onSaved: (value) {
+                      unitAmount = int.parse(value!);
+                    },
+                    textInputType: TextInputType.number,
+                    hintText: widget.productEntity.unitAmount.toString(),
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: CustomTextFormField(
+                    onSaved: (value) {
+                      numberOfCalories = int.parse(value!);
+                    },
+                    textInputType: TextInputType.number,
+                    hintText: widget.productEntity.numberOfCalories.toString(),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              onSaved: (value) {
+                productDescription = value!;
+              },
+              textInputType: TextInputType.text,
+              hintText: widget.productEntity.productDescription,
+              maxLines: 5,
+              hintPadding: const EdgeInsets.only(top: 32, right: 20),
+            ),
+            SizedBox(height: 16),
+            IsFeaturedField(
+              isChecked: isFeatured,
+              onChanged: (value) {
+                setState(() {
+                  isFeatured = value;
+                });
+              },
+            ),
+            SizedBox(height: 8),
+            IsOrganicField(
+              isChecked: isOrganic,
+              onChanged: (value) {
+                setState(() {
+                  isOrganic = value;
+                });
+              },
+            ),
+            SizedBox(height: 8),
+            EditImageSection(productEntity: widget.productEntity),
+            SizedBox(height: 16),
+            CustomButton(
+              text: "حفظ البيانات",
+              textStyle: AppStyles.bold13.copyWith(color: Colors.white),
+            ),
+            SizedBox(height: 16),
+            CustomButton(
+              text: "لا ارغب",
+              borderColor: ColorsData.kPrimaryColor,
+              backgroundColor: Colors.transparent,
+              textStyle: AppStyles.bold13.copyWith(
+                color: ColorsData.kPrimaryColor,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
