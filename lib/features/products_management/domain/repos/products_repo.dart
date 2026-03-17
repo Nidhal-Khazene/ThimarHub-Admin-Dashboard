76e4dd8eaac13e99621ad95e049e87c1fe36bf98
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-17 14:38:46 +0100

    products repo: add updateProduct method

--- lib/features/products_management/domain/repos/products_repo.dart
+++ lib/features/products_management/domain/repos/products_repo.dart
@@ -7,4 +7,8 @@ abstract class ProductsRepo {
   Future<Either<Failure, List<ProductEntity>>> getProducts();
 
   Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts();
+  Future<Either<Failure, void>> updateProduct({
+    required ProductEntity productEntity,
+    required String productCode,
+  });
 }
