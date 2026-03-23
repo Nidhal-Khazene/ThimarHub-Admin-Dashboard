5975113ad03de4bdd917b05981a83a408ba8d65f
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-23 22:01:53 +0100

    implement remove product cubit

--- lib/features/products_management/presentation/manager/cubits/remove_product_cubit/remove_product_cubit.dart
+++ lib/features/products_management/presentation/manager/cubits/remove_product_cubit/remove_product_cubit.dart
@@ -1,8 +1,33 @@
 import 'package:bloc/bloc.dart';
+import 'package:ecommerce_app_dashboard/features/products_management/domain/repos/images_repo.dart';
+import 'package:ecommerce_app_dashboard/features/products_management/domain/repos/products_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'remove_product_state.dart';
 
 class RemoveProductCubit extends Cubit<RemoveProductState> {
-  RemoveProductCubit() : super(RemoveProductInitial());
+  RemoveProductCubit(this.imagesRepo, this.productsRepo)
+    : super(RemoveProductInitial());
+  final ImagesRepo imagesRepo;
+  final ProductsRepo productsRepo;
+
+  Future<void> removeProduct({
+    required String productCode,
+    required String imagePath,
+  }) async {
+    emit(RemoveProductLoading());
+    final imageResult = await imagesRepo.deleteImage(imagePath);
+    imageResult.fold(
+      (failure) => emit(RemoveProductFailure(errMessage: failure.message)),
+      (_) async {
+        final productResult = await productsRepo.deleteProduct(
+          productCode: productCode,
+        );
+        productResult.fold(
+          (failure) => emit(RemoveProductFailure(errMessage: failure.message)),
+          (_) => emit(RemoveProductSuccess()),
+        );
+      },
+    );
+  }
 }
