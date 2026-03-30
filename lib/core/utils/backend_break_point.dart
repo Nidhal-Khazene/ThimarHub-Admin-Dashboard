5ee67a0152c8b3dfae8d0de7d9ad1ef431c2cef4
nidhal khazene <nidhal.khazene@gmail.com>
2026-03-30 11:23:17 +0100

    backend break point: add delete order breack point

--- lib/core/utils/backend_break_point.dart
+++ lib/core/utils/backend_break_point.dart
@@ -1,12 +1,16 @@
 class BackendBreakPoint {
   static const images = "images";
+  //User
   static const addUserData = "users";
   static const getUserData = "users";
   static const checkIFUserExists = "users";
+  //Products
   static const productCollections = "products";
   static const getProducts = "products";
   static const updateProducts = "products";
   static const deleteProducts = "products";
+  //Orders
   static const getOrders = "orders";
   static const updateOrders = "orders";
+  static const deleteOrders = "orders";
 }
